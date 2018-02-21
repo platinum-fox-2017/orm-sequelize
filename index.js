7e786dea83f2645f5f3a9f816c4127aae836665b
agungcaproex <agung.caproex@gmail.com>
2018-02-21 18:16:54 +0700

    release 1

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
-const sequelize = new sequelize()
 
+const Controller = require('./controller.js')
+
+const argv = process.argv
+const syntax = argv[2]
+const option = argv[3]
+const option2 = argv.slice(4)
+
+
+
+Controller.command(syntax, option, option2)
